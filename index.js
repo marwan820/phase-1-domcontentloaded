c3c517e1049a0e5dc28a6d946233287b2c9c4128
Marwan Rahim <marwan820@Marwan>
2023-05-03 15:11:47 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+  const text = document.querySelector("p") 
+  text.textContent = "This is really cool!"
+    })
+
+ 
+
+
